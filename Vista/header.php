c03154719887e4e54958b2103168af378d7b88e2
Hugo Flores <mralesshug@gmail.com>
2024-09-28 11:44:00 -0600

    first commit

---
+++ Vista/header.php
@@ -0,0 +1,29 @@
+<header> 
+            <nav>
+                <div class="l">
+                    <a href="#">
+                        <img src="Vista/icon/log.png" alt="">
+                    </a>
+                </div>
+
+               
+                <div class="t">
+                    <a href="informacion.html">Informacion</a>
+                </div>
+
+                <div class="t">
+                    <a href="riego.html">Funcionalidades</a>
+                </div>
+
+                <div class="t">
+                    <a href="#">Usuarios</a>
+                </div>
+
+                <div class="c">
+                    <a href="Vista/login.php">
+                        <img src="Vista/icon/out.png">
+                    </a>
+                </div>
+
+            </nav>
+        </header>
